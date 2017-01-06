8f5ca218fd5471352213c6daf764b2eac06a6279
Ethan Coyne <ethancoyne@Ethans-MacBook-Pro.local>
2017-01-06 11:03:46 -0600

    completed what-the-function assignment

--- app.js
+++ app.js
@@ -2,17 +2,28 @@ function Student(firstName, favoriteColor) {
   this.firstName = firstName;
   this.favoriteColor = favoriteColor;
 }
-
 /**
  * 1. Use the Student constructor function to create variables
  * that represent you and your coding partner(s).
  */
+
+var Ethan = new Student('Ethan', 'Green');
+
+
+var Tyler = new Student('Tyler', 'Blue');
+
+
+var Scott = new Student('Scott', 'Salmon');
+
 
 /**
   * 2. Create a function that takes one argument. That argument will
   * represent a single student object. The function will log the student's
   * name.
   */
+function user(student){
+  console.log(student.firstName);
+}
 
 /***** SWITCH PROGRAMMERS *****/
 
@@ -21,14 +32,21 @@ function Student(firstName, favoriteColor) {
   * represent a single student object. The function will log the student's
   * favoriteColor.
   */
-
+function userColor(student){
+  console.log(student.favoriteColor);
+}
 /**
   * 4. Put both students created in Step 1 into an array called students.
   */
-
+var studentArray = [Ethan, Tyler, Scott];
+console.log(studentArray);
 /***** SWITCH PROGRAMMERS *****/
 
 /**
  * 5. Loop over the students array and invoke both functions from Step 2 and
  * Step 3. You should see console logs for each students firstName and favoriteColor.
  */
+for(var i = 0; i < studentArray.length; i++) {
+  user(studentArray[i]);
+  userColor(studentArray[i]);
+}
